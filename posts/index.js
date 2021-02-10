d3b4d345cde7ee10cf6d4e68e34cd4a0b05a773d
bujosa <davidbujosa@gmail.com>
2021-02-10 04:58:11 -0400

    Fixed package json added nodemon script

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
@@ -8,7 +10,17 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log("Listening on port 4000");

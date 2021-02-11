daea3c9c874ed1354afcb17c26ee82a18f4bbca5
bujosa <davidbujosa@gmail.com>
2021-02-11 04:54:22 -0400

    Fixed comment list

--- client/src/components/comment/CommentList.js
+++ client/src/components/comment/CommentList.js
@@ -1,5 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { helperComment } from "../../helpers/helper";
 
-export const CommentList = () => {
-  return <div></div>;
+export const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async (postId) => {
+    const res = await helperComment(`posts/${postId}/comments`);
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData(postId);
+  }, [postId]);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };

33f03dd4fb8eb9d84ddcb3822fd94ad273db108e
bujosa <davidbujosa@gmail.com>
2021-02-11 02:48:38 -0400

    Added post create

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PostCreate } from "./PostCreate";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
---
+++ client/src/PostCreate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const PostCreate = () => {
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>Title</label>
+          <input className="form-control"></input>
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};

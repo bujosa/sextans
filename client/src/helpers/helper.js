3d9d4dd5c4a3644c05b6b491a7ccb788a7710325
bujosa <davidbujosa@gmail.com>
2021-02-12 05:12:39 -0400

    Fixed client helper

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 const baseUrl = process.env.REACT_APP_POST_URL;
+const baseUrlPost = process.env.REACT_APP_POST_QUERY_URL;
 const baseUrlComment = process.env.REACT_APP_COMMENT_URL;
 
 export const helper = async (endpoint, data, method = "GET") => {
-  const url = await `${baseUrl}${endpoint}`;
+  const url = `${baseUrl}${endpoint}`;
   if (method === "GET") {
-    const res = await axios.get(url);
+    const res = await axios.get(`${baseUrlPost}${endpoint}`);
     return res;
   } else {
     return await axios.post(url, data);

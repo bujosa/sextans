8b5b20c5cddc589eb592e4b0ceb138f975a759fc
bujosa <davidbujosa@gmail.com>
2021-02-12 04:43:23 -0400

    Added status on comment type

--- comments/index.js
+++ comments/index.js
@@ -21,7 +21,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -31,6 +31,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 

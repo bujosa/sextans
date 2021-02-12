fa2c0e848538659ee17cf3eaa32df202d43acb8a
bujosa <davidbujosa@gmail.com>
2021-02-12 04:48:49 -0400

    Added validation for CommentCreated

--- moderation/index.js
+++ moderation/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+});
 
 app.listen(4003, () => {
   console.log("Listening on port 4003");

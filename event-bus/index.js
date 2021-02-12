59a1ed5e6f90dce37017cf29622b7b36c9a4acc6
bujosa <davidbujosa@gmail.com>
2021-02-12 03:57:48 -0400

    Fixed query

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const cors = require("cors");
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
 app.post("/events", (req, res) => {
   const event = req.body;

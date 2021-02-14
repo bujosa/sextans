90451d7f62265c79025e54cfcb92fdf851c4f0c4
bujosa <davidbujosa@gmail.com>
2021-02-14 11:53:04 -0400

    Fixed moderation query and comments

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,15 +16,15 @@ app.post("/events", (req, res) => {
   axios.post("http://posts-service:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
   res.send({ status: "OK" });
 });
 
--- query/index.js
+++ query/index.js
@@ -48,7 +48,7 @@ app.post("/events", (req, res) => {
 
 app.listen(4002, async () => {
   console.log("Listening on port 4002");
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://events-service:4005/events");
   for (let event of res.data) {
     console.log("Processing event:", event.type);
     handleEvent(event.type, event.data);

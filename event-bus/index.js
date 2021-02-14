7f2a617847d7e26deff5e35de3dbeb80250cb173
bujosa <davidbujosa@gmail.com>
2021-02-14 12:01:23 -0400

    Fixed event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,15 +16,15 @@ app.post("/events", (req, res) => {
   axios.post("http://posts-service:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  // axios.post("http://localhost:4001/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
+  axios.post("http://comments-service:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-service:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-service:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 

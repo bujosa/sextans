7ebe1134357f8429735763bf6a82983bc9b1fd53
bujosa <davidbujosa@gmail.com>
2021-02-14 11:33:22 -0400

    Fixed and integrated new host

--- posts/index.js
+++ posts/index.js
@@ -24,7 +24,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://events-service:4005/events", {
     type: "PostCreated",
     data: {
       id,
